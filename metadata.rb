56dc528481b0e9fd38a25856d2e2211dfde69d6b
Myroslav Rys <mrys@softserveinc.com>
2013-07-05 14:48:16 +0300

    First working release

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'system-update'
+maintainer       'Myroslav Rys'
+maintainer_email 'stonevil@gmail.com'
+license          'Aapache 2.0'
+description      'Update package lists from the repositories and force upgrade system'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.0'
+
+%w{ ubuntu debian redhat centos fedora oracle suse }.each do |os|
+  supports os
+end
---
+++ recipes/upgrade.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: system-update
+# Recipe:: update
+#
+# Copyright (C) 2013 Myroslav Rys
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "system-update::update"
+
+case node["platform_family"]
+  when "debian"
+    execute "Force upgrade all packages" do
+      command "sudo apt-get -y upgrade"
+    end
+  when "rhel"
+    execute "Force upgrade all packages" do
+      command "sudo yum -y update"
+    end
+end
