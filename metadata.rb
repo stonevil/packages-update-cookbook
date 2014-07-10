01b38857e759b4170e404a3aadbbfb9b2e03ca74
Myroslav Rys <stonevil@gmail.com>
2014-07-10 20:47:27 +0300

    Update version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'stonevil@gmail.com'
 license          'Aapache 2.0'
 description      'Update package lists from the repositories and force upgrade system'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 %w{ ubuntu debian redhat centos fedora oracle suse }.each do |os|
   supports os
